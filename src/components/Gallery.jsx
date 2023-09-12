256c2a382d344e58c0856ccd03de8e4451bd8779
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2023-09-12 02:54:50 -0500

    Use data.json to display images

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,27 +1,47 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
-import imageData from './imageData';
+// import imageData from './imageData';
+import hornedBeastImageData from '../assets/data.json';
 
 class Gallery extends React.Component {
 
   render() {
 
-    let images = [];
+  //   let images = [];
 
-    imageData.forEach ( (image, index) => {
-      images.push (
-        <HornedBeast
-          key = {index}
-          title = {image.title}
-          imageUrl = {image.image_url}
-          description = {image.description}
-        />
-      );
-    });
+  //   hornedBeastImageData.forEach ( (image, index) => {
+  //     images.push (
+  //       <HornedBeast
+  //         key = {index}
+  //         title = {image.title}
+  //         imageUrl = {image.image_url}
+  //         description = {image.description}
+  //       />
+  //     );
+  //   });
 
-    return <div>{images}</div>
+  //   return <div>{images}</div>
+  // }
+
+    return (
+
+      <div>
+
+        {hornedBeastImageData.map ( (image, index) => 
+          <HornedBeast 
+            key = {index}
+            title = {image.title}
+            imageUrl = {image.image_url}
+            description = {image.description}
+          />
+        
+        )};
+
+      </div>
+
+    );
+
   }
-
 }
 
 export default Gallery;
