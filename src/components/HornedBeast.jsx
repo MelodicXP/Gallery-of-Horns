61a7ed3b52153c227dc9127a2df6607e3f662367
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2023-09-12 09:25:38 -0500

    Update image src url

--- src/components/HornedBeast.jsx
+++ src/components/HornedBeast.jsx
@@ -24,7 +24,7 @@ class HornedBeast extends React.Component {
 
     return (
       <>
-          <p className='heart-number'><img src="src/assets/heart.webp" alt="picture of heart" className='heart-img'/>{` ${this.state.totalFavorites}`}</p>
+          <p className='heart-number'><img src="https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA0L2pvYjY4NS0zMS1wLnBuZw.png?s=3mXWtJo-6hWc_PTad35orL_SHH2ZEzvu_MQjLYv_mMs" alt="picture of heart" className='heart-img'/>{` ${this.state.totalFavorites}`}</p>
           <h2>{this.props.title}</h2>
           <img className='horned-beast-img'src={this.props.imageUrl} alt={this.props.description} title ={this.props.title} />
           <p>{this.props.description}</p>
